9ce82be637c1f402c71ac804571f90021ee0c48f
Juan Antonio Castañeda Ramirez <castaneda2850@gmail.com>
2024-09-05 20:20:32 -0500

    Creé BrowserRouter, Creé el login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom"
 
+//pages
+import { Login } from "./pages/auth/Login";
+import { LayoutAdmin } from "./layouts/LayoutAdmin";
+import { Home } from "./pages/admin/Home";
+import { Error404 } from "./pages/Error404";
 
 function App() {
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/login" element = {<Login/>} />
+      <Route path="/" element= {<LayoutAdmin/>} >
+          <Route index element = {<Home/>}/>
+      </Route>
+      <Route path="*" element = {<Error404/>} />
+    </Routes>
+   </BrowserRouter>
   )
 }
 
